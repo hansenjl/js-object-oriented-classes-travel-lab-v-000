d7026d4dd24f326c50dae51da43a9a336baf4071
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-07-13 17:51:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,8 @@ class Driver {
 
 
 class Route {
-  
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
 }
